00510188830eeddffcee59b3f881e43fa01b7728
MiltonJ23 <jonhmilton1991@gmail.com>
2026-05-05 05:05:18 +0100

    feat: added the default hash cost, the hashpassword method to hash the pass, the one to compare hashed password and plain text and finally a method to check the dummy , it will serve later

---
+++ services/auth-service/pkg/crypto/bcrypt.go
@@ -0,0 +1,36 @@
+package crypto
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+// BcryptCost is fixed at 12 according to the exigence NFR-SEC-02	in the SRS
+const BcryptCost = 12
+
+// DummyHash is a valied hash generated at start to guarantee a constant execution time
+var DummyHash []byte
+
+func init() {
+	var err error
+	DummyHash, err = bcrypt.GenerateFromPassword([]byte("dummy_timing_protection_string"), BcryptCost)
+	if err != nil {
+		panic("failed to generate dummy hash for timing attack protection: " + err.Error())
+	}
+}
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), BcryptCost)
+	if err != nil {
+		return "", err
+	}
+	return string(bytes), nil
+}
+
+func ComparePasswordHashAndPassword(hash, password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+func DummyCheck(password string) {
+	_ = bcrypt.CompareHashAndPassword(DummyHash, []byte(password))
+}
